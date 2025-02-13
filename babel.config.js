f7aff33b5ff8af41beca95060b70ccdd6afaf2a1
Tanthaii <140239690+Tanthaii@users.noreply.github.com>
2025-02-13 16:02:40 +0700

    first commit

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,9 @@
-module.exports = function (api) {
-    api.cache(true);
-    return {
-      presets: ['babel-preset-expo'],
-      plugins: ['react-native-reanimated/plugin'], // ตัวอย่างการเพิ่มปลั๊กอิน
-    };
+module.exports = function(api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      'react-native-reanimated/plugin'
+    ]
   };
-  
+}
